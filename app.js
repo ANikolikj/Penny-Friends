2530086630544db1a26ecc849f4cb9dd005e42a3
ANikolikj <angela.nikolik@gmail.com>
2021-07-19 03:31:49 +0200

    more changes

--- app.js
+++ app.js
@@ -5,8 +5,26 @@
 //     console.log("Letter ${i} is ${logo[i].getTotalLength()}");
 // }
 
-function toggleText() {
+function toggleTextOne() {
     var text = document.getElementById("bubble1");
+    if (text.style.opacity === "0") {
+      text.style.opacity = "1";
+    } else {
+      text.style.opacity = "0";
+    }
+  }
+
+  function toggleTextTwo() {
+    var text = document.getElementById("bubble2");
+    if (text.style.opacity === "0") {
+      text.style.opacity = "1";
+    } else {
+      text.style.opacity = "0";
+    }
+  }
+
+  function toggleTextThree() {
+    var text = document.getElementById("bubble3");
     if (text.style.opacity === "0") {
       text.style.opacity = "1";
     } else {
